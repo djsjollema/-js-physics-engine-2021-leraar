ef83747198c5233f0636ce9c82ae32f9eb6ed115
djsjollema <j.sjollema@ma-web.nl>
2021-09-27 14:11:49 +0200

    point

--- html/02myPoint/script.js
+++ html/02myPoint/script.js
@@ -4,38 +4,29 @@ const context = canvas.getContext('2d');
 const width = window.innerWidth;
 const height = window.innerHeight;
 
+let myArray = [];
+
 canvas.width = width;
 canvas.height = height;
-
-////let point = new Point(100,100,30,"yellow");
-
-function drawCircle(){
-    /*
-        teken een honderd rode cirkels met als middelpunt
-        M(200,200) en een straal van 50 px
-    */
-
-    for(let i = 0; i<100; i++){
-        let x = Math.random() * width;
-        let y = Math.random() * height;
-        let radius = 50;
-        context.beginPath();
-        context.fillStyle = "rgb(255,0,0)";
-        context.arc(x,y,radius,0,2*Math.PI)
-        context.closePath();
-        context.stroke();
-        context.fill();
-    }
-
-}
 
 animate();
 
 function animate(){
     requestAnimationFrame(animate);
-    context.clearRect(0,0,width,height)
-    point.x += 1;
-    point.y += 1;
-    point.radius += 1;
-    point.draw();
+    context.clearRect(0,0,width,height);
+    if(Math.random()<0.01){
+        let x = -50;
+        let y = Math.random()*height;
+        let p = new Point(x,y,50,"yellow");
+        myArray.push(p);
+    }
+
+    for(let i = 0 ; i<myArray.length; i++){
+        myArray[i].x += 1;
+        myArray[i].draw();
+
+        if(myArray[i].x > width){
+            myArray.splice(i,1)
+        }
+    }
 }
--- library/Point.js
+++ library/Point.js
@@ -3,16 +3,18 @@ class Point {
         this.x = x;
         this.y = y;
         this.radius = radius;
-        this.color =color;
+        this.color = color;
+        
     }
 
     draw(){
         context.beginPath();
         context.fillStyle = this.color;
+        context.strokeStyle = "black";
+        context.lineWidth = "2"
         context.arc(this.x,this.y,this.radius,0,2*Math.PI)
         context.closePath();
         context.stroke();
         context.fill();
     }
 }
-
