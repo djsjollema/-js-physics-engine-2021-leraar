a5d023a7ecbb05f5373d7c30afbf136244a9eada
djsjollema <j.sjollema@ma-web.nl>
2021-10-11 14:25:05 +0200

    05 half

--- html/05linearFunction/script.js
+++ html/05linearFunction/script.js
@@ -10,14 +10,23 @@ let numberOfPoints = 4;
 canvas.width = width;
 canvas.height = height;
 
-let A,B,background;
+let A,B,background,l;
 
 A = new Point(200,200,20,"red",true);
 B = new Point(500,300,20,"blue",true);
 
+l=new LinearFunction(-1,400);
+
+for(let x = 0; x <500; x+=5){
+    console.log(l.y(x));
+    let p = new Point(x,l.y(x),2,"yellow");
+    p.draw();
+}
+
+
 background = new GraphBackGround();
 
-animate();
+//animate();
 
 function animate(){
     requestAnimationFrame(animate);
